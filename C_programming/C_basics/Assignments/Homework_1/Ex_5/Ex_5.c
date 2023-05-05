36f5cd95f0c60a27fc7249b2b18b0cb9e70607de
Abdelrahman Elsamahy <ali.elsamahy05@gmail.com>
2023-05-05 19:23:47 +0300

    Ex 5

---
+++ C_programming/C_basics/Assignments/Homework_1/Ex_5/Ex_5.c
@@ -0,0 +1,23 @@
+/*
+ ============================================================================
+ Name        : Ex_5.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	char x;
+
+		printf("Enter a character: ");
+		fflush(stdin);  fflush(stdout);
+		scanf("%c",&x);
+
+		printf("ASCII value of %c is %d",x,x);
+}

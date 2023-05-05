c1ae16d92c85b7849aa9b2d60ad15286cb6151d4
Abdelrahman Elsamahy <ali.elsamahy05@gmail.com>
2023-05-05 19:18:49 +0300

    final lab

---
+++ C_programming/C_basics/Labs/final_lecture_lab/final lab.c
@@ -0,0 +1,28 @@
+/*
+ ============================================================================
+ Name        : final.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+
+	int i , j;
+
+	for(i=0;i<10;i++)
+	{
+		for(j=0;j+i<10;j++)
+		{
+			printf("%d",j+i);
+		}
+		printf("\n");
+	}
+
+}

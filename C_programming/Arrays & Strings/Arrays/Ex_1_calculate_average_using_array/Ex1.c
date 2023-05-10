41d0e0ce0cc1f6efa889e0aa54eb3e4174c81460
Abdelrahman Elsamahy <ali.elsamahy05@gmail.com>
2023-05-11 00:29:46 +0300

    array

---
+++ C_programming/Arrays & Strings/Arrays/Ex_1_calculate_average_using_array/Ex1.c
@@ -0,0 +1,46 @@
+/*
+ ============================================================================
+ Name        : Ex1.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	float a[2][2] , b[2][2] , c[2][2];
+	int i,j;
+	printf("Enter the Elements of 1st matrix:\n");
+	for(i=0;i<2;i++)
+	{
+		for(j=0;j<2;j++)
+		{
+			printf("Enter a%d%d : ",i,j);
+			fflush(stdin);  fflush(stdout);
+			scanf("%f",&a[i][j]);
+		}
+	}
+	printf("Enter the Elements of 2st matrix:\n");
+		for(i=0;i<2;i++)
+		{
+			for(j=0;j<2;j++)
+			{
+				printf("Enter a%d%d : ",i,j);
+				fflush(stdin);  fflush(stdout);
+				scanf("%f",&b[i][j]);
+			}
+		}
+		printf("sum of matrix: \n  ");
+			for(i=0;i<2;i++)
+			{
+				for(j=0;j<2;j++)
+				{
+					c[i][j]=a[i][j]+b[i][j];
+					printf("%0.1f \t",c[i][j]);
+				}
+				printf("\n");
+			}
+}

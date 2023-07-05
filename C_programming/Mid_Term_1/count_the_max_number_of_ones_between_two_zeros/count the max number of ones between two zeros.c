7b4127fa668f467c57395d22b444edcc963174a0
Abdelrahman Elsamahy <ali.elsamahy05@gmail.com>
2023-07-05 04:07:44 +0300

    .

---
+++ C_programming/Mid_Term_1/count_the_max_number_of_ones_between_two_zeros/count the max number of ones between two zeros.c
@@ -0,0 +1,48 @@
+/*
+ ============================================================================
+ Name        : count.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+unsigned int Count_binary (unsigned int number);
+
+int main(void) {
+	unsigned int number ;
+	printf("Enter a Number: ");
+	fflush(stdin);   fflush(stdout);
+	scanf("%d",&number);
+
+	printf("Max Number of ones between two zeros in %d is: %d", number, Count_binary(number));
+}
+unsigned int Count_binary (unsigned int number)
+{
+	int i , k , count=0 , j=0;
+	printf("Binary number of %d is : ",number);
+	for(i=7 ; i>=0 ; i--)
+	{
+		k = number >> i;
+		if(k & 1)
+		{
+		printf("1" );
+		count++;
+		}
+		else
+		{
+			printf("0" );
+
+			if(count > j){
+			j=count;
+			count=0;
+			}
+		}
+	}
+	printf("\n");
+	return j;
+}
